e2ec3627f96f0a58cea8bc6c3ba52dd73dc702e0
Daniel <danielcoliveira9@gmail.com>
2024-10-22 17:14:52 +0100

    MediaSalario

---
+++ NomedoMaisVelho/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Introduza o nome da primeira pessoa: ");
+            string p1nome = Console.ReadLine();
+       
+            Console.WriteLine("Introduza a idade: ");
+            int p1idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o nome da segunda pessoa: ");
+            string p2nome = Console.ReadLine();
+
+            Console.WriteLine("Introduza a idade: ");
+            int p2idade = int.Parse(Console.ReadLine());
+
+            if (p1idade < p2idade)
+            {
+                Console.WriteLine("A pessoa mais velha é : " + p2nome);
+            }
+            else if (p1idade > p2idade)
+            {
+                Console.WriteLine("A pessoa mais velha é: " + p1nome);
+            }
+            else
+            {
+                Console.WriteLine("São da mesma idade!");
+            }
+           
+            Console.ReadKey();
+
+        }
+    }
+}
