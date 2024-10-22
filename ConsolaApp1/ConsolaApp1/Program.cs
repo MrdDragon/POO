8f44eb05bc8887f95615b56ce3c802861567fe74
Daniel <danielcoliveira9@gmail.com>
2024-10-22 17:11:54 +0100

    ConsoleApp

---
+++ ConsolaApp1/ConsolaApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsolaApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
