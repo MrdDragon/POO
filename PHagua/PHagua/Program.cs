0363f4d55b4d36eb0ca1c26144fabbfeeef91374
Daniel <danielcoliveira9@gmail.com>
2024-11-12 10:10:04 +0000

    PHagua

---
+++ PHagua/PHagua/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace PHagua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtph = 0;
+            double phagua;
+
+            Console.Write("\nQuantidade de aguas a analizar: ");
+            qtph = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= qtph; i++)
+            {
+                Console.Write("\nIntroduza o PH a analizar: ");
+                phagua = double.Parse(Console.ReadLine());
+
+                if (phagua < 7)
+                {
+                    Console.WriteLine("A água é ácida!");
+                }
+                else if (phagua > 7)
+                {
+                    Console.WriteLine("A água é alcalina!");
+                }
+                else
+                {
+                    Console.WriteLine("A água é neutra!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
